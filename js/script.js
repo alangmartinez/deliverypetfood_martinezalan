43cf10862e1734c2a1342f600ce93d56dbc86093
Alan Martinez <alannmartinezz@icloud.com>
2021-12-14 20:16:21 -0300

    Se soluciona visualizacion de nav bar para desktop.

--- js/script.js
+++ js/script.js
@@ -5,11 +5,14 @@ let btnMenu = document.getElementById("btnMenu");
 let menuSide = document.getElementById("menuSide");
 let body = document.getElementById("body");
 let userIcon = document.getElementById("userIcon");
+let main = document.getElementById("main");
 
 
 btnMenu.addEventListener("click", function(){
 
+    if(window.innerWidth >= 1280)
         body.classList.toggle("noScroll");
         menuSide.classList.toggle("mostrar");
         userIcon.classList.toggle("oculto");
-});
+        main.classList.toggle("moveRight");
+    });
