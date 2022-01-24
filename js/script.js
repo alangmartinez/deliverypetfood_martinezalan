38cb5bbe7c78c3fbe96a5f719bd7c2ace2537922
Alan Martinez <alannmartinezz@icloud.com>
2022-01-24 09:58:46 -0300

    se modifica market.html y help.html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 // Loader spin
 
-
-window.onload = function(){
+onload = ()=>{
     const loader = document.getElementById("loaderContainer");
     loader.style.opacity = 0;
     loader.style.visibility = "hidden";
     loader.style.position = "absolute";
+    loader.style.zIndex = "-1";
 }
-
-
 
 // Evento para desplazar menu side al hacer "click" en btnMenu.
 
@@ -19,7 +17,7 @@ let userIcon = document.getElementById("userIcon");
 let main = document.getElementById("main");
 
 
-btnMenu.addEventListener("click", function(){
+btnMenu.addEventListener("click", ()=>{
     body.classList.toggle("noScroll");
     menuSide.classList.toggle("mostrar");
     userIcon.classList.toggle("oculto");
@@ -29,8 +27,28 @@ let btnUser = document.getElementById("btnUser");
 let loginUser = document.getElementById("login");
 let login = false;
 
-if(login == false){
-    btnUser.addEventListener("click", function(){
-        loginUser.classList.toggle("mostrarLogin");
-    });
-}
+// if(login == false){
+//     btnUser.addEventListener("click", function(){
+//         loginUser.classList.toggle("mostrarLogin");
+//     });
+// }
+
+btnUser.addEventListener("click", ()=> {
+    if(login == false){
+        loginUser.style.transform = "translateX(0)";
+        loginUser.style.opacity = "1";
+        login = true;
+
+    }
+    else{
+        loginUser.style.transform = "translateX(100%)";
+        loginUser.style.opacity = "0";
+        login = false;
+    }
+})
+
+const form = document.getElementById("form");
+
+form.addEventListener("click", (e)=>{
+    e.preventDefault();
+})
